0d4071ed18d046a87c803a6c181d196e19ce151c
Jed <jed@m6700.local>
2020-03-02 15:16:28 -0500

    Added relative path

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-cd ../simulinkdevops.github.io
-mkdocs gh-deploy --config-file ../simulinkdevops.github.io_src/mkdocs.yml --remote-branch master
+mkdocs gh-deploy --config-file ./mkdocs.yml --remote-branch master
